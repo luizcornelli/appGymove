37da23cb42bb605f7604319dd1e3578474cc6266
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-06-06 04:07:41 -0300

    Adding style

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text } from 'react-native';
+import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <KeyboardAvoidingView>
+    <KeyboardAvoidingView style={styles.background}>
 
-      <View>
+      <View style={styles.containerLogo}>
         <Image
           source={require('./src/assets/appGymoveLogo.png')}
         />
       </View>
 
-      <View>
+      <View style={styles.container}>
 
-        <TextInput
+        <TextInput style={styles.input}
           placeholder="email"
           autoCorrect={false}
           onChangeText={() => { }}
         />
 
-        <TextInput
+        <TextInput style={styles.input}
           placeholder="senha"
           autoCorrect={false}
           onChangeText={() => { }}
         />
 
-        <TouchableOpacity>
-          <Text>Acessar</Text>
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
-          <Text>Criar conta</Text>
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta</Text>
         </TouchableOpacity>
 
       </View>
@@ -38,3 +38,57 @@ export default function App() {
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1, // capturando todo o tamanho da tela
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#191919'
+  },
+
+  containerLogo: {
+    flex: 1,
+    justifyContent: 'center' // alinhando verticalmente
+  },
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50
+  },
+
+  input: {
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10
+  },
+
+  btnSubmit: {
+    backgroundColor: '#35aaff',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+
+  submitText: {
+    color: '#fff',
+    fontSize: 18
+  },
+
+  btnRegister: {
+    marginTop: 10,
+  },
+
+  registerText: {
+    color: '#fff'
+  }
+})
